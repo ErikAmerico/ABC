fbe07fd053543c0617f353ffa9535c201ceb76fa
Erik Olson <erikamerico1991@gmail.com>
2023-09-26 21:31:37 -0400

    configured main.jsx and app.jsx, added home page and header component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from './pages/home.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <h1>Oops! This page does not exist.</h1>,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);
